c5b6640df301a4b6fa861971c00f9786a7f11261
Mona Lisa <mona.lisa@behind.you>
2022-01-01 22:12:16 -0800

    Add liquidation and bankruptcy logs

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,42 @@
 use anchor_lang::prelude::*;
+use std::fmt;
 
+// == ZO ==
+#[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone, Debug)]
+pub enum LiquidationEvent {
+    Perp,
+    Spot,
+}
+impl fmt::Display for LiquidationEvent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[event]
+#[derive(Clone)]
+pub struct LiquidationLog {
+    pub liquidation_event: LiquidationEvent,
+    pub base_symbol: String, // for Perp, this is the market symbol
+    pub quote_symbol: Option<String>, // for Perp, None
+    pub liqor_margin: Pubkey,
+    pub liqee_margin: Pubkey,
+    pub assets_to_liqor: i64,
+    pub quote_to_liqor: i64,
+}
+
+#[event]
+pub struct BankruptcyLog {
+    pub base_symbol: String,
+    pub liqor_margin: Pubkey,
+    pub liqee_margin: Pubkey,
+    pub assets_to_liqor: i64,
+    pub quote_to_liqor: i64,
+    pub insurance_loss: i64,
+    pub socialized_loss: i64,
+}
+
+// == DEX ==
 #[event]
 pub struct RealizedPnlLog {
     pub market_key: Pubkey,
